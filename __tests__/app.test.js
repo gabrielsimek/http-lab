a6fedebcb257f309b4415e868e9a2da8fd86a059
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-14 17:44:02 -0700

    first route request handler

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+  it.only('gets "hi" from / ', async() => {
+    const res = await request(app).get('/');
+  
+    expect(res.text).toEqual('hi');
+  });
 });
+  
