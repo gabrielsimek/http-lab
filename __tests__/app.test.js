5c7db06492f21d8544851534e429d3057927e7d5
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-14 19:37:32 -0700

    get /green test red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,23 +15,41 @@ describe.only('app routes', () => {
     expect(res.status).toBe(200);
     expect(res.text).toEqual('hello');
   });
-  it.only('gets a html page with h1 and word red from /red', async() => {
+  it('gets a html page with h1 and word red from /red', async() => {
     const res = await request(app)
       .get('/red');
 
     expect(res.status).toBe(200);
     expect(res.text).toEqual(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    </head>
-    <body>
-      <h1>Red</h1>
-    </body>
-    </html>`);
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Red</h1>
+</body>
+</html>`);
+  });
+  it('gets a html page with h1 and word green from /green', async() => {
+    const res = await request(app)
+      .get('/green');
+
+    expect(res.status).toBe(200);
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>green</h1>
+</body>
+</html>`);
   });
 });
   
