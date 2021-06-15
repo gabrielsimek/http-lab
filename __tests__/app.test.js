a23a3f1438a5299a5c23b9717c5ce7df6c498619
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-14 18:03:46 -0700

    handle /red test red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,12 +8,30 @@ describe.only('app routes', () => {
     expect(res.text).toEqual('hi');
   });
 
-  it.only('returns status 200 and plain text req body at post /echo', async() => {
+  it('returns status 200 and plain text req body at post /echo', async() => {
     const res = await request(app)
       .post('/echo')
       .send('hello');
     expect(res.status).toBe(200);
     expect(res.text).toEqual('hello');
+  });
+  it.only('gets a html page with h1 and word red from /red', async() => {
+    const res = await request(app)
+      .get('/red');
+
+    expect(res.status).toBe(200);
+    expect(res.text).toEqual(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>Red</h1>
+    </body>
+    </html>`);
   });
 });
   
