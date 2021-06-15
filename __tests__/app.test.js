68c4ac99696808ed8c0ef326706640186400d9e4
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-14 17:58:40 -0700

    post /echo handler and test past

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('app routes', () => {
-  it.only('gets "hi" from / ', async() => {
+describe.only('app routes', () => {
+  it('gets "hi" from / ', async() => {
     const res = await request(app).get('/');
   
     expect(res.text).toEqual('hi');
+  });
+
+  it.only('returns status 200 and plain text req body at post /echo', async() => {
+    const res = await request(app)
+      .post('/echo')
+      .send('hello');
+    expect(res.status).toBe(200);
+    expect(res.text).toEqual('hello');
   });
 });
   
