822cf804cf8e00b2cb53d36527cce8349830672e
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-17 10:40:01 -0700

    refactor with optional-chaining

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -125,3 +125,22 @@ describe('file system', () => {
   });
 });
   
+
+
+
+
+// '<!DOCTYPE html>\r' +
+//     '<html lang="en">\r\n' +
+//     '\r\n' +
+//     '<head>\r\n' +
+//     '  <meta charset="UTF-8">\r\n' +
+//     '  <meta http-equiv="X-UA-Compatible" content="IE=edge">\r\n' +
+//     '  <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n' +
+//     '  <title>Document</title>\r\n' +
+//     '</head>\r\n' +
+//     '\r\n' +
+//     '<body>\r\n' +
+//     '  <h1> red </h1>\r\n' +
+//     '</body>\r\n' +
+//     '\r\n' +
+//     '</html>'
